b46415d62e2b467aa699808e701d81936fc31a97
matthew <g00430349@atu.ie>
2024-09-29 21:09:13 +0100

    added method called add

--- src/main/java/atu/ie/calC.java
+++ src/main/java/atu/ie/calC.java
@@ -1,7 +1,25 @@
 package atu.ie;
 
+import java.util.Scanner;
+
 public class calC {
     public static void main(String[] args) {
+    //methods
+        System.out.println("please enter the first number: ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("please enter the second number: ");
+        int b = sc.nextInt();
+        System.out.println("please enter an operation: ");
+        String op = sc.next();
+        switch (op) {
+            case "add": add(a, b); break;
+            default: System.out.println("invalid operation"); break;
+        }
 
+    }
+    public static void add(int a, int b) {
+        int sum = a + b;
+        System.out.println("The sum is: " + sum);
     }
 }
